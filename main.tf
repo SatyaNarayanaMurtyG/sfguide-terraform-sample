93b9daf0da0855d371ddff370737eb749b94284b
SatyaNarayanaMurtyG <Satyanarayana.g@tigeranalytics.com>
2025-09-29 19:58:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,10 +17,7 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_warehouse" "wh_ref_us_0003" {
-  name                 = "WH_REF_US-0003"
-  warehouse_size       = "SMALL"
-  auto_suspend         = 3
-  auto_resume          = true
-  initially_suspended  = true
+resource "snowflake_database" "demo_db1" {
+  name    = "DEMO_DB1"
+  comment = "Database for Snowflake Terraform demo"
 }
