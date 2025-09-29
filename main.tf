986f998eb4af913f1fea93a19b646c0711fff41f
SatyaNarayanaMurtyG <Satyanarayana.g@tigeranalytics.com>
2025-09-30 00:01:26 +0530

    updated main and variable

--- main.tf
+++ main.tf
@@ -1,20 +1,16 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "snowflakedb/snowflake"
-    }
-  }
-
-  backend "remote" {
-    organization = "Satya_Tiger_Org"
-
-    workspaces {
-      name = "gh-actions-demo"
+      source = "snowflakedb/snowflake"
     }
   }
 }
 
 provider "snowflake" {
+  organization_name = var.SNOWFLAKE_ORG_NAME # required if not using profile. Can also be set via SNOWFLAKE_ORGANIZATION_NAME env var
+  account_name      = var.SNOWFLAKE_ACC_NAME # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT_NAME env var
+  user              = var.SNOWFLAKE_USER # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
+  password          = var.SNOWFLAKE_PASSWORD  
 }
 
 resource "snowflake_database" "demo_db1" {
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "SNOWFLAKE_ACC_NAME" {
+    type        = string
+}
+variable "SNOWFLAKE_ORG_NAME" {
+    type        = string
+}
+variable "SNOWFLAKE_PASSWORD" {
+  sensitive = true
+}
+variable "SNOWFLAKE_USER" {
+    type        = string
+}
