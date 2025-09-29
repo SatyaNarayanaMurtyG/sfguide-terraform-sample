114e636084c227d80f0359c2521bdeaa4c9eebae
SatyaNarayanaMurtyG <Satyanarayana.g@tigeranalytics.com>
2025-09-30 00:10:01 +0530

    update 2

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "SNOWFLAKE_ACC_NAME" {
-    type        = string
+  type = string
 }
 variable "SNOWFLAKE_ORG_NAME" {
-    type        = string
+  type = string
 }
 variable "SNOWFLAKE_PASSWORD" {
   sensitive = true
 }
 variable "SNOWFLAKE_USER" {
-    type        = string
+  type = string
 }
