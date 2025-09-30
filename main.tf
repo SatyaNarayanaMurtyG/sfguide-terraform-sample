0d91b3af15ada7281dd63eb9fe83e7edd6f7d4cd
SatyaNarayanaMurtyG <Satyanarayana.g@tigeranalytics.com>
2025-09-30 13:13:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,16 @@ terraform {
 }
 
 provider "snowflake" {
+  organization_name = SNOWFLAKE_ORGANIZATION_NAME
+  account_name      = SNOWFLAKE_ACCOUNT_NAME
+  user              = SNOWFLAKE_USER
+  authenticator     = "PROGRAMMATIC_ACCESS_TOKEN"
+  token             = var.token
+}
+
+variable "token" {
+  type      = string
+  sensitive = true
 }
 
 resource "snowflake_database" "demo_db1" {
