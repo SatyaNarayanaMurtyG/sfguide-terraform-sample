5f61242a629b5c98d73d0091d73561da386a315f
SatyaNarayanaMurtyG <Satyanarayana.g@tigeranalytics.com>
2025-09-30 15:16:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,11 +7,5 @@ terraform {
 }
 
 provider "snowflake" {
-  organization_name = var.organization_name
-  account_name      = var.account_name
-  host              = var.host
-  user              = var.user
-  password          = var.password
-  role              = var.role
-  warehouse         = var.warehouse
+
 }
